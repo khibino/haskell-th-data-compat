77d1f6d3262ba2690547a1861d835057f550186f
Kei Hibino <ex8k.hibino@gmail.com>
2016-06-04 14:43:05 +0900

    add interfaces to destruct.

--- src/Language/Haskell/TH/Compat/Data/Current.hs
+++ src/Language/Haskell/TH/Compat/Data/Current.hs
@@ -1,10 +1,15 @@
 module Language.Haskell.TH.Compat.Data.Current (
-  dataD', newtypeD', dataInstD', newtypeInstD',
+  dataD', unDataD,
+  newtypeD', unNewtypeD,
+  dataInstD', unDataInstD,
+  newtypeInstD', unNewtypeInstD,
   ) where
 
 import Language.Haskell.TH
-  (dataD, newtypeD, dataInstD, newtypeInstD, conT,
-   CxtQ, Name, TyVarBndr, ConQ, TypeQ, DecQ)
+  (CxtQ, ConQ, TypeQ, DecQ,
+   Cxt, Con, Type, Name, TyVarBndr, Kind,
+   Dec (DataD, NewtypeD, DataInstD, NewtypeInstD),
+   dataD, newtypeD, dataInstD, newtypeInstD, conT)
 
 
 -- | Definition against 'dataD',
@@ -13,11 +18,21 @@ dataD' :: CxtQ -> Name -> [TyVarBndr] -> [ConQ] -> [Name]
        -> DecQ
 dataD' cxt n bs cs ds = dataD cxt n bs Nothing cs $ mapM conT ds
 
+-- | Compatible interface to destruct DataD
+unDataD :: Dec -> Maybe (Cxt, Name, [TyVarBndr], Maybe Kind, [Con], [Type])
+unDataD (DataD cxt n bs mk cs ds) = Just (cxt, n, bs, mk, cs, ds)
+unDataD  _                        = Nothing
+
 -- | Definition against 'newtypeD',
 --   compatible with before temaplate-haskell-2.11
 newtypeD' :: CxtQ -> Name -> [TyVarBndr] -> ConQ -> [Name]
           -> DecQ
 newtypeD' cxt n bs c ds = newtypeD cxt n bs Nothing c $ mapM conT ds
+
+-- | Compatible interface to destruct NewtypeD
+unNewtypeD :: Dec -> Maybe (Cxt, Name, [TyVarBndr], Maybe Kind, Con, [Type])
+unNewtypeD (NewtypeD cxt n bs mk c ds) = Just (cxt, n, bs, mk, c, ds)
+unNewtypeD  _                          = Nothing
 
 -- | Definition against 'dataInstD',
 --   compatible with before temaplate-haskell-2.11
@@ -25,8 +40,18 @@ dataInstD' :: CxtQ -> Name -> [TypeQ] -> [ConQ] -> [Name]
            -> DecQ
 dataInstD' cxt n as cs ds = dataInstD cxt n as Nothing cs $ mapM conT ds
 
+-- | Compatible interface to destruct DataInstD
+unDataInstD :: Dec -> Maybe (Cxt, Name, [Type], Maybe Kind, [Con], [Type])
+unDataInstD (DataInstD cxt n as mk cs ds) = Just (cxt, n, as, mk, cs, ds)
+unDataInstD  _                            = Nothing
+
 -- | Definition against 'newtypeInstD',
 --   compatible with before temaplate-haskell-2.11
 newtypeInstD' :: CxtQ -> Name -> [TypeQ] -> ConQ -> [Name]
               -> DecQ
 newtypeInstD' cxt n as c ds = newtypeInstD cxt n as Nothing c $ mapM conT ds
+
+-- | Compatible interface to destruct NewtypeInstD
+unNewtypeInstD :: Dec -> Maybe (Cxt, Name, [Type], Maybe Kind, Con, [Type])
+unNewtypeInstD (NewtypeInstD cxt n as mk c ds) = Just (cxt, n, as, mk, c, ds)
+unNewtypeInstD  _                              = Nothing
--- src/Language/Haskell/TH/Compat/Data/V210.hs
+++ src/Language/Haskell/TH/Compat/Data/V210.hs
@@ -1,10 +1,15 @@
 module Language.Haskell.TH.Compat.Data.V210 (
-  dataD', newtypeD', dataInstD', newtypeInstD',
+  dataD', unDataD,
+  newtypeD', unNewtypeD,
+  dataInstD', unDataInstD,
+  newtypeInstD', unNewtypeInstD,
   ) where
 
 import Language.Haskell.TH
-  (dataD, newtypeD, dataInstD, newtypeInstD,
-   CxtQ, Name, TyVarBndr, ConQ, TypeQ, DecQ)
+  (CxtQ, ConQ, TypeQ, DecQ,
+   Cxt, Con, Type (ConT), Name, TyVarBndr, Kind,
+   Dec (DataD, NewtypeD, DataInstD, NewtypeInstD),
+   dataD, newtypeD, dataInstD, newtypeInstD)
 
 
 -- | Definition against 'dataD',
@@ -13,11 +18,21 @@ dataD' :: CxtQ -> Name -> [TyVarBndr] -> [ConQ] -> [Name]
        -> DecQ
 dataD' = dataD
 
+-- | Compatible interface to destruct DataD
+unDataD :: Dec -> Maybe (Cxt, Name, [TyVarBndr], Maybe Kind, [Con], [Type])
+unDataD (DataD cxt n bs cs ds) = Just (cxt, n, bs, Nothing, cs, map ConT ds)
+unDataD  _                     = Nothing
+
 -- | Definition against 'newtypeD',
 --   compatible with before temaplate-haskell-2.11
 newtypeD' :: CxtQ -> Name -> [TyVarBndr] -> ConQ -> [Name]
           -> DecQ
 newtypeD' = newtypeD
+
+-- | Compatible interface to destruct NewtypeD
+unNewtypeD :: Dec -> Maybe (Cxt, Name, [TyVarBndr], Maybe Kind, Con, [Type])
+unNewtypeD (NewtypeD cxt n bs c ds) = Just (cxt, n, bs, Nothing, c, map ConT ds)
+unNewtypeD  _                       = Nothing
 
 -- | Definition against 'dataInstD',
 --   compatible with before temaplate-haskell-2.11
@@ -25,8 +40,18 @@ dataInstD' :: CxtQ -> Name -> [TypeQ] -> [ConQ] -> [Name]
            -> DecQ
 dataInstD' = dataInstD
 
+-- | Compatible interface to destruct DataInstD
+unDataInstD :: Dec -> Maybe (Cxt, Name, [Type], Maybe Kind, [Con], [Type])
+unDataInstD (DataInstD cxt n as cs ds) = Just (cxt, n, as, Nothing, cs, map ConT ds)
+unDataInstD  _                         = Nothing
+
 -- | Definition against 'newtypeInstD',
 --   compatible with before temaplate-haskell-2.11
 newtypeInstD' :: CxtQ -> Name -> [TypeQ] -> ConQ -> [Name]
               -> DecQ
 newtypeInstD' = newtypeInstD
+
+-- | Compatible interface to destruct NewtypeInstD
+unNewtypeInstD :: Dec -> Maybe (Cxt, Name, [Type], Maybe Kind, Con, [Type])
+unNewtypeInstD (NewtypeInstD cxt n as c ds) = Just (cxt, n, as, Nothing, c, map ConT ds)
+unNewtypeInstD  _                           = Nothing
